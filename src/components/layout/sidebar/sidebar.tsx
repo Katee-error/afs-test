78b72fcbc7ae0b6130f69205ca3773d588c9393e
Ekaterina <kate8190758@gmail.com>
2025-04-02 19:42:51 +0400

    feat: add icons, update design sidebar

--- src/components/layout/sidebar/sidebar.tsx
+++ src/components/layout/sidebar/sidebar.tsx
@@ -1,45 +1,56 @@
-import React from 'react';
-import styles from './Sidebar.module.scss';
-import Link from 'next/link';
+"use client";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import companyIcon from "./../../../../public/assets/icons/Company.svg";
+import contractorIcon from "./../../../../public/assets/icons/Contractor.svg";
+import accountIcon from "./../../../../public/assets/icons/Account.svg";
+import styles from "./Sidebar.module.scss";
+import { usePathname } from "next/navigation";
 
-interface Props {
-  className?: string;
-}
+export const Sidebar = () => {
+  const pathname = usePathname();
 
-export const Sidebar: React.FC<Props> = ({ className }) => {
+  const navItems = [
+    { href: "/", label: "Organizations", icon: companyIcon },
+    { href: "/contractors", label: "Contractors", icon: contractorIcon },
+    { href: "/clients", label: "Clients", icon: accountIcon },
+  ];
+
   return (
     <aside className={styles.sidebar}>
-    <div className={styles.brand}>
-      <h2>Oak Tree Cemetery</h2>
-      <p>Process Manager</p>
-    </div>
+      <div className={styles.brand}>
+        <h2>Oak Tree Cemetery</h2>
+        <p>Process Manager</p>
+      </div>
 
-    <nav className={styles.nav}>
-      <ul>
-        <li className={styles.active}>
-          <Link href="/organizations">
-            <span className={styles.icon} />
-            <span>Organizations</span>
-          </Link>
-        </li>
-        <li>
-          <Link href="/contractors">
-            <span className={styles.icon} />
-            <span>Contractors</span>
-          </Link>
-        </li>
-        <li>
-          <Link href="/clients">
-            <span className={styles.icon} />
-            <span>Clients</span>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+      <div className={styles.drawer}></div>
 
-    <div className={styles.footer}>
-      <p>All Funeral Services © 2019-2025</p>
-    </div>
-  </aside>
+      <nav className={styles.nav}>
+        <ul>
+          {navItems.map((item) => {
+            const isActive = pathname === item.href;
+            return (
+              <li key={item.href} className={isActive ? styles.active : ""}>
+                <Link href={item.href} className={styles.navLink}>
+                  <Image
+                    src={item.icon}
+                    width={20}
+                    height={20}
+                    alt={`${item.label} icon`}
+                    className={isActive ? styles.lightIcon : styles.darkIcon}
+                  />
+                  <span>{item.label}</span>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </nav>
+
+      <div className={styles.footer}>
+        <p>All Funeral Services © 2015-2025</p>
+      </div>
+    </aside>
   );
 };
