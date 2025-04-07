2643256da7cb3e901c69d3024f393cd1db92db03
Ekaterina <kate8190758@gmail.com>
2025-04-08 00:49:08 +0400

    fix: fix problems deploy

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,23 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
+  output: "export",
+  basePath: isProd ? "/afs-test" : "",
+  assetPrefix: isProd ? "/afs-test/" : "",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
         hostname: "test-task-api.allfuneral.com",
-        pathname: "**", 
+        pathname: "**",
       },
+      
     ],
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 };
 
 module.exports = nextConfig;
-
